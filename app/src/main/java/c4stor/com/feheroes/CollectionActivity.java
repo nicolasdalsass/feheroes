b06b8d24790198e32d0f108fc7a4cb0077141b80
Nicolas Dalsass <Nicolas Dalsass>
2017-02-20 00:36:27 +0100

    v27

--- app/src/main/java/c4stor/com/feheroes/CollectionActivity.java
+++ app/src/main/java/c4stor/com/feheroes/CollectionActivity.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
